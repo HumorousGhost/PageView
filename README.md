69301f7b5b0a84f16bb00dcf8c2aa16205500a33
v-zhangliqun <v-zhangliqun@360.cn>
2023-01-17 16:32:29 +0800

    first

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 # PageView
 
-A description of this package.
+Page turning function similar to ScrollView.
+
+## Supported Platforms
+* iOS 13.0
+* macOS 10.15
+* tvOS 13.0
+* watchOS 6.0
+
+## Usage
+```swift
+import PageView
+
+struct ContentView: View {
+    let textArray = ["Hello", "world", "Hello world"]
+    @State private var index: Int = 0
+    
+    var body: some View {
+        PageView(count: self.textArray.count, currentIndex: $index) {
+            ForEach(self.textArray, id: \.self) { text in
+                textView(text: text)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func textView(text: String) -> some View {
+        Text(text)
+            .padding()
+    }
+}
+```
+
+## Installation
+You can add PageView to an Xcode project by adding it as a package dependency
+
+* From the **File** menu, select **Swift Packages** -> **Add Package Dependency**...
+Enter https://github.com/HumorousGhost/PageView into the package repository URL text field.
+Link **PageView** to your application target.
--- Sources/PageView/PageView.swift
+++ Sources/PageView/PageView.swift
@@ -1,6 +1,84 @@
-public struct PageView {
-    public private(set) var text = "Hello, World!"
 
-    public init() {
+import SwiftUI
+import ChangeObserver
+
+public struct PageView<Content>: View where Content: View {
+    
+    let count: Int
+    @State private var ignore: Bool = false
+    @State private var currentFloatIndex: CGFloat = 0 {
+        didSet {
+            ignore = true
+            currentIndex = min(max(Int(currentFloatIndex.rounded()), 0), self.count)
+            ignore = false
+        }
+    }
+    
+    let content: Content
+    
+    @GestureState private var translation: CGFloat = 0
+    
+    /// initialization method
+    /// - Parameters:
+    ///   - count: total number of pages
+    ///   - currentIndex: The number of pages currently displayed
+    ///   - content: some View
+    public init(count: Int, currentIndex: Binding<Int>, content: () -> Content) {
+        self.count = count
+        self._currentIndex = currentIndex
+        self.content = content()
+    }
+    
+    @Binding var currentIndex: Int {
+        didSet {
+            if !ignore {
+                currentFloatIndex = CGFloat(currentIndex)
+            }
+        }
+    }
+    
+    public var body: some View {
+        GeometryReader { geometry in
+            HStack(spacing: 0) {
+                self.content
+                    .frame(width: geometry.size.width)
+            }
+            .frame(width: geometry.size.width, alignment: .leading)
+            .offset(x: -CGFloat(currentIndex) * geometry.size.width)
+            .offset(x: self.translation)
+            .animation(.interactiveSpring())
+            .gesture(dragGesture(geometry.size.width))
+            .onDataChange(of: currentIndex) { value in
+                withAnimation(.easeOut) {
+                    currentFloatIndex = CGFloat(value)
+                }
+            }
+            .onDataChange(of: self.translation) { newValue in
+                debugPrint(newValue)
+            }
+        }
+    }
+    
+    private func dragGesture(_ width: CGFloat) -> some Gesture {
+        DragGesture()
+            .updating(self.$translation) { value, state, _ in
+                state = value.translation.width
+            }
+            .onEnded { value in
+                let offset = value.translation.width / width
+                let offsetPredicted = value.predictedEndTranslation.width / width
+                let newIndex = (CGFloat(self.currentIndex) - offset).rounded()
+                self.currentIndex = min(max(Int(newIndex), 0), self.count - 1)
+                
+                withAnimation(.easeOut) {
+                    if offsetPredicted < -0.5 && offset > -0.5 {
+                        self.currentFloatIndex = CGFloat(min(max(Int(newIndex.rounded() + 1), 0), self.count - 1))
+                    } else if offsetPredicted > 0.5 && offset < 0.5  {
+                        self.currentFloatIndex = CGFloat(min(max(Int(newIndex.rounded() - 1), 0), self.count - 1))
+                    } else {
+                        self.currentFloatIndex = CGFloat(min(max(Int(newIndex.rounded()), 0), self.count - 1))
+                    }
+                }
+            }
     }
 }
